40582e2eb5cc6f652d771d3a60e61118e82c0fe4
Eduardo Wentancur <94117653+eduwentancur@users.noreply.github.com>
2022-05-26 16:35:27 -0300

    agregando los modal

--- src/main/java/com/demo/ecommerce/controller/ProductController.java
+++ src/main/java/com/demo/ecommerce/controller/ProductController.java
@@ -4,10 +4,12 @@ package com.demo.ecommerce.controller;
 import com.demo.ecommerce.models.Product;
 import com.demo.ecommerce.repository.ProductRepository;
 import java.util.List;
+import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,6 +51,17 @@ public class ProductController {
         producto.setDescription(description);
         productRepo.save(producto);
         return "redirect:/listProducts.html";
+    }
+    
+    
+    @GetMapping("/deleteProd/{id}" )
+    public String deleteProduct(@PathVariable("id") Long id){
+        Product producto = productRepo.findById(id).get();
+        productRepo.delete(producto);
+        
+        
+        return "redirect:/listProducts.html";
+    
     }
     
 }
